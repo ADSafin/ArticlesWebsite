a925d9701c49a32d38cdb3a00999e660798b3d12
ASafin <aynursafin88@gmail.com>
2024-08-10 15:44:38 +0300

    Сверстал шаблон для страниц, главную страницу, страницу статьи

--- WebArticles/articles/urls.py
+++ WebArticles/articles/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('article', views.article)
+    path('', views.main, name='main'),
+    path('authorization', views.authorization, name='authorization'),
+    path('registration', views.registration, name='registration'),
+    path('article', views.article, name='article'),
+    path('addarticle', views.addarticle, name='add article'),
+    path('editarticle', views.editarticle, name='edit article'),
 ]
--- WebArticles/articles/views.py
+++ WebArticles/articles/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-def index(request):
+def main(request):
     return render(request, 'articles/main.html')
 
 def article(request):
-    return HttpResponse("<h4> статья  </h4>")
+    return render(request, 'articles/article.html')
+
+def addarticle(request):
+    return HttpResponse("<h4> добавление статьи </h4>")
+
+def editarticle(request):
+    return HttpResponse("<h4> редактирование статьи  </h4>")
+
+def authorization(request):
+    return HttpResponse("<h4> вход  </h4>")
+
+def registration(request):
+    return HttpResponse("<h4> регистрация  </h4>")
 
